9539e5d0475f3960956cf50f9de587cf0140e80d
Gobelinus <silent.arora@gmail.com>
2019-07-10 17:10:47 +0530

    [part 1] - course information, step 2 complete

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -8,22 +8,11 @@ const Header = () => {
   )
 }
 
-const Content = (props) => {
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
+const Part = (props) => {
   return (
-    <>
-      <p>
-        {part1} {props.exercises1}
-      </p>
-      <p>
-        {part2} {props.exercises2}
-      </p>
-      <p>
-        {part3} {props.exercises3}
-      </p>
-    </>
+    <p>
+      {props.name} {props.exercises}
+    </p>
   )
 }
 
@@ -37,10 +26,15 @@ const App = () => {
   const exercises1 = 10
   const exercises2 = 7
   const exercises3 = 14
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
   return (
     <>
       <Header />
-      <Content exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3} />
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
       <Total exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3} />
     </>
   )
