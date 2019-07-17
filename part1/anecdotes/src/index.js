586747ad035606499c398ccecd9c7631fc77f734
Gobelinus <silent.arora@gmail.com>
2019-07-17 12:23:11 +0530

    [part 1] - 1.13*: anecdotes step 2 - anecdote with votes with manual click

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -10,26 +10,40 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
 const App = (props) => {
   const points = new Array(anecdotes.length).fill(0)
-
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState([...points])
 
   const getNextAnecdotes = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
-    const newVotes = [...votes]
-    newVotes[selected] += 1
-    setVotes([...newVotes])
+  }
+
+  const updateVotes = (selected, currentVotes) => {
+    return () => {
+      const newVotes = [...currentVotes]
+      newVotes[selected] += 1
+      setVotes([...newVotes])
+    }
+
   }
 
   return (
     <>
-        <div>
-          {props.anecdotes[selected]}
-        </div>
-        <p>has {votes[selected]} votes</p>
-        <button onClick={getNextAnecdotes}>next anecdote</button>
+      <Header text="Anecdote of the day" />
+      <div>
+        {props.anecdotes[selected]}
+      </div>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={updateVotes(selected, votes)}>vote</button>
+      <button onClick={getNextAnecdotes}>next anecdote</button>
+
     </>
   )
 }
